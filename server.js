b75e659206aba099fb2d43558f1a894bc142dcad
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-02 20:08:01 +0700

    test2

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'))
 
 app.listen(process.env.PORT || 8000);
