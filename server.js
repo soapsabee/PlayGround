7565f4454db2b353cd6258487dadbc7edbabf005
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-05 21:10:59 +0700

    test6

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => res.render('pages/index'))
-
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  
 app.listen(process.env.PORT || 8000);
