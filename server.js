f9a56117100a4cdb010dbecf5d257e8819e341a1
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-08 17:51:25 +0700

    test24

--- server.js
+++ server.js
@@ -23,22 +23,21 @@ app.get('/db', async (req, res) => {
     }
   })
 
-app.post('/insert_pd/add', function (req, res,next) {
+app.post('/insert_pd/add', function (req, res) {
 
-  pool.connect(function (err,client,done){
-    if(err){
-      console.log("not able to get connection" +err);
-      res.status(400).send(err);
-    }
-    client.query('insert into product_table(title,price,create_at) values ("test2","50","10/8/2018")',function(err,result){
-      done();
-      if(err){
-        console.log(err);
-        res.status(400).send(err);
-      }
-      res.status(200).send(result.rows);
-    });
-  });
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM product_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+  
 });
 
 app.get('/insert_pd',(req,res) => res.render('pages/insert_pd'));
