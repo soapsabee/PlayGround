2f99f415b5b028328d03109f44eb6e161179c7ed
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-15 18:04:07 +0700

    10/15/2018

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.get('/db', async (req, res) => {
       const client = await pool.connect()
       const result = await client.query(`SELECT * FROM product_table where id = ${pid}`);
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/edit_pd', results );
+      console.log(results.id);
+      // res.render('pages/edit_pd', results );
       client.end();
     } catch (err) {
       console.error(err);
