c135f6bed2c28c5c994755d51f151f59f647d812
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-15 17:54:24 +0700

    10/15/2018

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/complete_del/:id', async (req, res) =>{
 
 //app.post('/complete_del',(req,res) => res.render('pages/complete_del'));  
 app.get('/insert_pd',(req,res) => res.render('pages/insert_pd'));
-//app.get('/edit_pd',(req,res) => res.render('pages/edit_pd'));
+app.get('/edit_pd',(req,res) => res.render('pages/edit_pd'));
 
 app.listen(process.env.PORT || 8000);
 
