bf4bee684c4e65dcb2637e039ad72169f3a2705d
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-15 18:11:15 +0700

    10/15/2018

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/db', async (req, res) => {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM product_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.send('pages/db'+ results );
+      res.send('pages/db'+ result );
       client.end();
     } catch (err) {
       console.error(err);
