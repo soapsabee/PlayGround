cc81f2528bf3f73534b7c0d550ca3c07ac330ac8
soapsabee <s5930213055@phuket.psu.ac.th>
2018-10-21 01:22:41 +0700

    10/21/2018 setup purchase_report

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ app.get('/purchase_report',  async (req, res) => {
     const result2 = await client.query(`select sum(sale) from purchase group by to_char(buy_at,'MM');`);
     const results = { 'results': (result) ? result.rows : null};
     const results2 = { 'results2': (result2) ? result2.rows : null};
-    res.send(results,results2);
+    res.send(results2);
     client.end();
   } catch (err) {
     console.error(err);
