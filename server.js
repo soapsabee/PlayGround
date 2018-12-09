c21e99a4436864aab3b9c799f2fe8a0e50b94ce4
soapsabee <s5930213055@phuket.psu.ac.th>
2018-12-10 00:34:44 +0700

    12/10/2018

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 var bodyParser = require('body-parser')
 let date = require('date-and-time');
+const axios = require('axios');
 const app = express();
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
@@ -15,35 +12,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/product_list', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM product_table ORDER BY id ASC');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/product_list', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+app.get('/', (req, res) => {
+
+  axios.get('http://localhost:8080/api/alldetails')
+  .then(function (response) {
+    let product = [];
+    response.data.map((posts)=>{
+
+      product.push(posts);
+    })
+    res.render('pages/index',{posts: product});
+    console.log("id1"+product[1].app);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
   })
   
+  
+app.get('/show_apps',(req, res) =>{
+ 
+  axios.get('http://localhost:8080/api/alldetails')
+  .then(function (response) {
+    let product = [];
+    response.data.map((posts)=>{
 
-
-  app.get('/product_list/:id',  async (req, res) => {
-    var pid = req.params.id;
-     try {
-      const client = await pool.connect()
-      const result = await client.query(`SELECT * FROM product_table where id = ${pid}`);
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/edit_pd', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+      product.push(posts);
+    })
+    res.render('pages/show_apps', {posts : product});
+   
   })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  
+})
+
+
+});
+
 
 
 app.listen(process.env.PORT || 8000);
